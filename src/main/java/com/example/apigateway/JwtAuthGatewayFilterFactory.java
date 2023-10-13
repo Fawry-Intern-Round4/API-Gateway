eb3e60149f0cfa47db33dfb34d4dd1568710560f
Amr Mousa <amrmousa1682@gmail.com>
2023-10-13 13:51:06 +0300

    refactor auth filter

--- src/main/java/com/example/apigateway/JwtAuthGatewayFilterFactory.java
+++ src/main/java/com/example/apigateway/JwtAuthGatewayFilterFactory.java
@@ -2,20 +2,22 @@ package com.example.apigateway;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.function.Function;
 
 @Component
 public class JwtAuthGatewayFilterFactory extends AbstractGatewayFilterFactory<JwtAuthGatewayFilterFactory.Config> {
-    private final  WebClient.Builder webClient;
+    private final WebClient.Builder webClient;
 
-    public JwtAuthGatewayFilterFactory( WebClient.Builder webClient) {
+    public JwtAuthGatewayFilterFactory(WebClient.Builder webClient) {
         super(Config.class);
         this.webClient = webClient;
     }
@@ -28,29 +30,34 @@ public class JwtAuthGatewayFilterFactory extends AbstractGatewayFilterFactory<Jw
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
+
             String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
-            final String token = (Objects.isNull(authHeader) || !authHeader.startsWith("Bearer "))
-                    ? "" : authHeader.substring(7);
+            String token;
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                token = authHeader.substring(7);
+            } else {
+                token = "";
+            }
             return webClient.build().get()
                     .uri("lb://user-api/token/validation")
                     .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
                     .exchangeToMono(clientResponse -> {
-                        if (clientResponse.statusCode().isError()) {
-                            return clientResponse.bodyToMono(String.class)
-                                    .flatMap(error -> {
-                                        exchange.getResponse().setStatusCode(clientResponse.statusCode());
-                                        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
-                                        return exchange.getResponse().writeWith(
-                                                Mono.just(exchange.getResponse().bufferFactory()
-                                                        .wrap(error.getBytes())));
-                                    });
-
+                        if (clientResponse.statusCode().equals(HttpStatus.OK)) {
+                            return chain.filter(exchange);
                         }
-                        return chain.filter(exchange);
+                        Function<String, Mono<Void>> errorFun = error -> {
+                            ServerHttpResponse response = exchange.getResponse();
+                            response.setStatusCode(clientResponse.statusCode());
+                            response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+                            byte[] errorBytes = error.getBytes();
+                            return response.writeWith(Mono.just(response.bufferFactory().wrap(errorBytes)));
+                        };
+                        return clientResponse.bodyToMono(String.class).flatMap(errorFun);
                     });
         };
 
     }
+
     @Validated
     public static class Config {
 
